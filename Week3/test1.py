bfc183e7c1e42641ab9a0079ddd521ca7c6d01cd
hliza84 <hovhannisyanliza84@gmail.com>
2023-03-17 20:00:33 +0400

    changes after lesson

--- Week3/test1.py
+++ Week3/test1.py
@@ -8,20 +8,20 @@ print(seconds)
 """   
 n=11
 sum=0
-sum=int(n//10)+int(n%10)
+sum=n//10+n%10
 print(sum)
 
 """2. A repdigit is a positive number composed out of the same digit. Create a function that takes an two-digit integer and returns whether it's a repdigit or not.
 """
-n=44
-x=int(n/10)==int(n%10)
+n=-55
+x=n//10==n%10
 print(x)
 
 """4.Create a function that takes the age in years and returns the age in days.
 """
-years=0
-age=int(years)
-days=age*365
+year=1960
+age=2023-1960
+days=age*365+age//4
 print(days)
 
 """6.Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet.
@@ -32,11 +32,10 @@ print(feet)
 
 """7.Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
 """
-x=15
-if x%2==0:
-    print("even")
-else:
-    print("odd")
+x=-10
+y=x%2
+a="even"*(y==0)+"odd"*y
+print(a)
 
 """ 5.Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
 """
@@ -44,9 +43,26 @@ hours=1.3
 minutes = hours * 60;
 seconds = minutes * 60;
 print(seconds);
-                                                                                         
+        
 
-                                                                                         
+
+
+year=1960
+age=2023-1960
+days=age*365+age//4
+print(days)
+                                                 
+year=1993
+age=2023-year
+a="վայելիր"*(age>=20 and age<=30)+"ուշ է"*(age>30 and age<=40)+"գնա հետո կգաս"*(age<20 or age>40)
+print(a)
+                                
+                                
+                                
+input_data=input("year of birth,please")
+year=int(input_data)
+print(year)                    
+                                
 
 
 
